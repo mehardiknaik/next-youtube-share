967ea96afa67b6a5b066e04f48e72d65afbafff7
Hardik Naik <96820742+mehardiknaik@users.noreply.github.com>
2023-10-29 17:37:48 +0530

    intial commit

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import { Box } from "@mui/material";
 
 export default function Home() {
   return (
-    <Box>
+    <Box marginTop={1}>
       <Player />
     </Box>
   );
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -5,12 +5,12 @@ import ReactPlayer from "react-player";
 
 const Player = () => {
   return (
-    <Box display={{ sm: "flex" }}>
+    <Box display={{ md: "flex" }} gap={1}>
       <Box
         sx={{
           flex: 1,
           position: "relative",
-          paddingTop: "56.25%",
+          paddingTop: { xs: "56.25%", md: "28.12%" },
           "& .player": {
             position: "absolute",
             top: 0,
@@ -23,6 +23,7 @@ const Player = () => {
           url="https://www.youtube.com/watch?v=LXb3EKWsInQ"
           width="100%"
           height="100%"
+          fallback={<div>asdcwa..</div>}
         />
       </Box>
       <Box flex={1}>scas</Box>
